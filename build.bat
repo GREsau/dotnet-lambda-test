54cd1aeaa8218d570d9b37d584d64ad59b3e9f26
Graham Esau <graham.esau@newvoicemedia.com>
2019-06-09 16:25:27 +0100

    Use custom runtime

--- build.bat
+++ build.bat
@@ -1,1 +1,15 @@
-sam build
+docker create --rm -it --name builder mcr.microsoft.com/dotnet/core/sdk:3.0
+docker start builder
+
+docker cp src/. builder:/src
+docker exec -w /src builder dotnet publish -c Release -o /out -r linux-x64
+docker exec -w /out builder chmod +x HelloWorld
+
+docker exec builder apt-get -qq update
+docker exec builder apt-get -qq install -y --no-install-recommends zip
+docker exec builder mkdir -p /dist
+docker exec -w /out builder sh -c "zip -r /dist/package.zip *"
+
+docker cp builder:/dist/. dist
+
+docker stop builder
--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
@@ -8,10 +13,35 @@ namespace HelloWorld
     {
         public static Task Main(string[] args)
         {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? EnvironmentName.Development;
+
+            if (environmentName.Equals(EnvironmentName.Development, StringComparison.OrdinalIgnoreCase))
+            {
+                return RunLocalWebHost(args);
+            }
+            else
+            {
+                return RunLambdaWebHost();
+            }
+        }
+
+        private static Task RunLocalWebHost(string[] args)
+        {
             var builder = WebHost.CreateDefaultBuilder(args);
             return ConfigureWebHost(builder)
                 .Build()
                 .RunAsync();
+        }
+
+        private static async Task RunLambdaWebHost()
+        {
+            var lambda = new LambdaFunction();
+            Func<APIGatewayProxyRequest, ILambdaContext, Task<APIGatewayProxyResponse>> handler = lambda.FunctionHandlerAsync;
+            using (var handlerWrapper = HandlerWrapper.GetHandlerWrapper(handler, new JsonSerializer()))
+            using (var bootstrap = new LambdaBootstrap(handlerWrapper))
+            {
+                await bootstrap.RunAsync();
+            }
         }
 
         public static IWebHostBuilder ConfigureWebHost(IWebHostBuilder builder) =>
